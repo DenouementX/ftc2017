6eb4c355989169ad4980d54e615d52f0e1d64740
Lawrence <DenouementX@users.noreply.github.com>
2017-10-02 16:37:28 -0700

    //test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KPSS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KPSS.java
@@ -1,37 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
 /**
- * Created by lawrencemao on 9/28/17.
+ * Created by KPSS on 26.09.17.
  */
-                    import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-                    import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-                    import com.qualcomm.robotcore.hardware.DcMotor;
-                    import com.qualcomm.robotcore.hardware.DcMotorSimple;
-                    import com.qualcomm.robotcore.util.Range;
+
+// test
 
 @TeleOp
 public class KPSS extends OpMode{
+
     DcMotor right;
     DcMotor left;
-    DcMotor left1;
-    DcMotor right1;
+
     public void init() {
+
         right = hardwareMap.dcMotor.get ("right");
         left = hardwareMap.dcMotor.get ("left");
-        right1 = hardwareMap.dcMotor.get ("right1");
-        left1 = hardwareMap.dcMotor.get ("left1");
+        left.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
     public void loop ()
     {
-        float left_power = gamepad1.left_stick_y;
-        float left1_power = gamepad1.left_stick_x;
-        float right_power = gamepad1.right_stick_y;
-        float right1_power = gamepad1.right_stick_x;
+
+        float left_power = -gamepad1.left_stick_y;
+        float right_power = -gamepad1.right_stick_y;
+
+        left_power = Range.clip(left_power, -1, 1);
+        right_power = Range.clip(right_power, -1, 1);
 
         left.setPower(left_power);
-        left1.setPower(left1_power);
         right.setPower(right_power);
-        right1.setPower(right1_power);
+
     }
 }
