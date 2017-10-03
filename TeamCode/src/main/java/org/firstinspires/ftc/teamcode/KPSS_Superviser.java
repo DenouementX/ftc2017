56d8b9a79b41577688259ea4b8294fd62ca63467
Artem <artemborisov17@gmail.com>
2017-10-03 15:56:52 -0700

    Test code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KPSS_Superviser.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+/**
+ * Created by artembookpro on 03.10.17.
+ */
+
+public class KPSS_Superviser extends OpMode{
+    ColorSensor color_sensor;
+    public void init()
+    {
+        color_sensor = hardwareMap.colorSensor.get ("color");
+
+    }
+while (color_sensor.alpha() < 20) {
+
+    }
+}
