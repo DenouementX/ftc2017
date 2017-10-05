81e12280c5b373c8af33c80fd9ec97d7855c2d3e
Lawrence <DenouementX@users.noreply.github.com>
2017-10-05 16:32:48 -0700

    Servo Test, I will replace code later

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue.java
@@ -1,13 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by lawrencemao on 10/5/17.
  */
 
-@Autonomous
-public class Auto_Blue{
+@TeleOp
+public class Auto_Blue extends OpMode{
 
+    Servo left;
+
+    public void init(){
+        left = hardwareMap.servo.get("left");
+    }
+
+    public void loop(){
+        if (gamepad1.a) {
+            left.setPosition(1);
+        }
+        else{
+            left.setPosition(0);
+        }
+    }
 
 }
