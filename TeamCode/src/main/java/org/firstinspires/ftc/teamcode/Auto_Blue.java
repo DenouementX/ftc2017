c16cba5a4405b376ebf8a2279ead392fe55c24f7
Lawrence <DenouementX@users.noreply.github.com>
2017-10-05 16:02:46 -0700

    Claw_Prototype initial commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
 /**
  * Created by lawrencemao on 10/5/17.
  */
 
-public class Auto_Blue {
+@Autonomous
+public class Auto_Blue{
+
+
 }
