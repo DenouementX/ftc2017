67be59f975e104d47d2b10c9ae1a959fd9fe0035
kevinwang <PhosphorusElement15@gmail.com>
2017-10-23 16:39:06 -0700

    //Added code for drive, strafing, and rotating the mecanum drive.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice_TeleOp.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
  * Created by kevinwang on 10/20/17.
  */
 
-@TeleOp(name = "Practice_Autonomous")
+@TeleOp(name = "Practice_TeleOp")
 
 public class Practice_TeleOp extends LinearOpMode{
 
