28a27e9d6a452c4c15d54d528f3820dbac1deb36
Artem <artemborisov17@gmail.com>
2017-10-26 15:20:24 -0700

    Auto_Blue Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-/* Created by KPSS*/
+/* Created by KPSS */
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -15,20 +15,18 @@ public class Auto_Blue extends OpMode{
     Servo servo;
 
     DcMotor front_left;
-    /*
     DcMotor front_right;
     DcMotor back_left;
     DcMotor back_right;
-    */
+
     public void init(){
         servo = hardwareMap.servo.get ("servo");
         color_sensor = hardwareMap.colorSensor.get("colorsensor");
+
         front_left = hardwareMap.dcMotor.get ("front_left");
-        /*
         front_right = hardwareMap.dcMotor.get ("front_right");
         back_left = hardwareMap.dcMotor.get ("back_left");
         back_right = hardwareMap.dcMotor.get ("back_right");
-        */
     }
 
     public void loop () {
@@ -36,29 +34,27 @@ public class Auto_Blue extends OpMode{
 
         if (color_sensor.blue()/2 > color_sensor.red()) {
             front_left.setPower(1);
-                    /*
+
                     front_right.setPower(1);
                     back_left.setPower(1);
                     back_left.setPower(1);
-                    */
+
         }
         if (color_sensor.blue() < color_sensor.red()/2) {
             front_left.setPower(-1);
-            /*
+
             front_right.setPower(-1);
             back_left.setPower(-1);
             back_left.setPower(-1);
-            */
         }
 
         else{
+
             servo.setPosition(0);
-            /*
             front_left.setPower(0);
             front_right.setPower(0);
             back_left.setPower(0);
             back_left.setPower(0);
-            */
         }
         servo.setPosition(0);
     }
