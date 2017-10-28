c2c55c3c6c22b162a20edd3584502292279945e8
Lawrence <DenouementX@users.noreply.github.com>
2017-10-27 18:08:02 -0700

    auto_red update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red.java
@@ -33,54 +33,80 @@ public class Auto_Red extends LinearOpMode {
         Servo jewel;
         ColorSensor color;
         State state;
+        DcMotor LF;
+        DcMotor LB;
+        DcMotor RF;
+        DcMotor RB;
+        int position = 0;
 
-        jewel = hardwareMap.servo.get("jewel");
-        color = hardwareMap.colorSensor.get("color");
+        state = State.INITIALIZE;
 
-        //setting up camera and vuforia
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        switch (state){
 
-        parameters.vuforiaLicenseKey = "AXzW9CD/////AAAAGTPAtr9HRUXZmowtd9p0AUwuXiBVONS/c5x1q8OvjMrQ8/XJGxEp0TP9Kl8PvqSzeXOWIvVa3AeB6MyAQboyW/Pgd/c4a4U/VBs1ouUsVBkEdbaq1iY7RR0cjYr3eLwEt6tmI37Ugbwrd5gmxYvOBQkGqzpbg2U2bVLycc5PkOixu7PqPqaINGZYSlvUzEMAenLOCxZFpsayuCPRbWz6Z9UJfLeAbfAPmmDYoKNXRFll8/jp5Ie7iAhSQgfFggWwyiqMRCFA3GPTsOJS4H1tSiGlMjVzbJnkusPKXfJ0dK3OH9u7ox9ESpi91T0MemXw3nn+/6QRvjGtgFH+wMDuQX7ta89+yW+wqdXX9ZQu8BzY";
+            case INITIALIZE:
 
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+                state = state.MOVE;
+                jewel = hardwareMap.servo.get("jewel");
+                color = hardwareMap.colorSensor.get("color");
+                LF = hardwareMap.dcMotor.get("LF");
+                LB = hardwareMap.dcMotor.get("LB");
+                RF = hardwareMap.dcMotor.get("RF");
+                RB = hardwareMap.dcMotor.get("RB");
 
-        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        VuforiaTrackable relicTemplate = relicTrackables.get(0);
-        relicTemplate.setName("relicVuMarkTemplate");
+                //setting up camera and vuforia
+                int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+                VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
 
-        telemetry.addData(">", "Press Play to start");
-        telemetry.update();
-        waitForStart();
+                parameters.vuforiaLicenseKey = "AXzW9CD/////AAAAGTPAtr9HRUXZmowtd9p0AUwuXiBVONS/c5x1q8OvjMrQ8/XJGxEp0TP9Kl8PvqSzeXOWIvVa3AeB6MyAQboyW/Pgd/c4a4U/VBs1ouUsVBkEdbaq1iY7RR0cjYr3eLwEt6tmI37Ugbwrd5gmxYvOBQkGqzpbg2U2bVLycc5PkOixu7PqPqaINGZYSlvUzEMAenLOCxZFpsayuCPRbWz6Z9UJfLeAbfAPmmDYoKNXRFll8/jp5Ie7iAhSQgfFggWwyiqMRCFA3GPTsOJS4H1tSiGlMjVzbJnkusPKXfJ0dK3OH9u7ox9ESpi91T0MemXw3nn+/6QRvjGtgFH+wMDuQX7ta89+yW+wqdXX9ZQu8BzY";
 
-        relicTrackables.activate();
+                parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+                this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
 
-        jewel.setPosition(1.0);
+                VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+                VuforiaTrackable relicTemplate = relicTrackables.get(0);
+                relicTemplate.setName("relicVuMarkTemplate");
 
-        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
-        //Params LEFT CENTER RIGHT into integers
-        if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-            telemetry.addData("VuMark", "%s visible", vuMark);
-            if (vuMark == RelicRecoveryVuMark.LEFT) {
-                int pos = 1;
-            }
-            if (vuMark == RelicRecoveryVuMark.CENTER) {
-                int pos = 2;
-            }
-            if (vuMark == RelicRecoveryVuMark.RIGHT) {
-                int pos = 3;
-            } else {
-                int pos = 0;
-            }
-        } else {
-            telemetry.addData("VuMark", "not visible");
-        }
+                telemetry.addData(">", "Press Play to start");
+                telemetry.update();
+                waitForStart();
 
-        while (opModeIsActive()) {
-            telemetry.addData("RED: ", color.red());
-            telemetry.addData("BLUE: ", color.blue());
+                relicTrackables.activate();
 
+                jewel.setPosition(1.0);
+
+                RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+                //Params LEFT CENTER RIGHT into integers
+                while (vuMark != RelicRecoveryVuMark.LEFT || vuMark != RelicRecoveryVuMark.CENTER || vuMark !=RelicRecoveryVuMark.RIGHT) {
+                        telemetry.addData("VuMark", "not visible");
+                        telemetry.update();
+                }
+
+                telemetry.addData("VuMark", "%s visible", vuMark);
+                telemetry.addData("RED: ", color.red());
+                telemetry.addData("BLUE: ", color.blue());
+
+                if (vuMark == RelicRecoveryVuMark.LEFT) {
+                    position = 1;
+                }
+                if (vuMark == RelicRecoveryVuMark.CENTER) {
+                    position = 2;
+                }
+                if (vuMark == RelicRecoveryVuMark.RIGHT) {
+                    position = 3;
+                }
+                telemetry.addData("Pos: ", position);
+
+                break;
+
+            case MOVE:
+                //state = state.CHECK;
+                break;
+            case CHECK:
+                //if (this.getTime() > 1000) state = state.STOP;
+                break;
+            case STOP:
+                //this.StopDrive();
+                break;
         }
     }
 }
