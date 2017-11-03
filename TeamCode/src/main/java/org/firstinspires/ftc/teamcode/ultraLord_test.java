a1d9758d8bc92039d8d7d11cd69d5be451a0a19f
Artem <artemborisov17@gmail.com>
2017-11-03 15:42:11 -0700

    ultraLord_Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultraLord_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultraLord_test.java
@@ -11,22 +11,23 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class ultraLord_test extends OpMode{
-    DcMotor lift;
+    //DcMotor lift;
     Servo rServo;
     Servo lServo;
-    double lPos = 1;
-    double rPos = 1;
+    double lPos;
+    double rPos;
 
     public void init(){
-        lift = hardwareMap.dcMotor.get("lift");
+        //lift = hardwareMap.dcMotor.get("lift");
         lServo = hardwareMap.servo.get("lS");
         rServo = hardwareMap.servo.get("rS");
     }
-
     public void loop(){
-        lift.setPower(gamepad1.left_stick_y);
-        lServo.setPosition(lPos);
-        rServo.setPosition(rPos);
+        //lift.setPower(gamepad1.left_stick_y);
+        lServo.setPosition(gamepad1.right_stick_y);
+        rServo.setPosition(gamepad1.left_stick_y);
+        lPos = lServo.getPosition();
+        rPos = lServo.getPosition();
         telemetry.addData("Left Servo: ", lPos);
         telemetry.addData("Right Servo: ", rPos);
     }
