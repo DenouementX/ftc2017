823f11c6d4788c620061a58c9e19c5232ceaff27
kevinwang <PhosphorusElement15@gmail.com>
2017-11-08 15:50:35 -0800

    New code with encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Encoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Encoders.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import com.sun.tools.javac.code.Attribute;
 
@@ -13,47 +14,72 @@ import com.sun.tools.javac.code.Attribute;
 
 @TeleOp(name = "TeleOp Encoders")
 
-public class TeleOp_Encoders extends LinearOpMode{
+public class TeleOp_Encoders extends LinearOpMode {
 
-    DcMotor motorfrontLeft;
-    DcMotor motorbackLeft;
-    DcMotor motorfrontRight;
-    DcMotor motorbackRight;
+    DcMotor fL;
+    DcMotor bL;
+    DcMotor fR;
+    DcMotor bR;
+    DcMotor lS;
+    Servo left;
+    Servo right;
 
     public void runOpMode() throws InterruptedException{
 
-        motorfrontLeft = hardwareMap.dcMotor.get("frontLeft");
-        motorbackLeft = hardwareMap.dcMotor.get("backLeft");
-        motorfrontRight = hardwareMap.dcMotor.get("frontRight");
-        motorbackRight = hardwareMap.dcMotor.get("backRight");
+        fL = hardwareMap.dcMotor.get("frontLeft");
+        bL = hardwareMap.dcMotor.get("backLeft");
+        fR = hardwareMap.dcMotor.get("frontRight");
+        bR = hardwareMap.dcMotor.get("backRight");
+        lS = hardwareMap.dcMotor.get("linearSlide");
+        left = hardwareMap.servo.get("left");
+        right = hardwareMap.servo.get("right");
 
-        motorfrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorbackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorfrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorbackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fL.setDirection(DcMotor.Direction.REVERSE);
+        left.setDirection(Servo.Direction.REVERSE);
 
-        motorfrontLeft.setDirection(DcMotor.Direction.REVERSE);
-        //motorbackLeft.setDirection(DcMotor.Direction.REVERSE);
+        fL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
         while(opModeIsActive()) {
 
-            double POWER = Range.clip(Math.max(Range.clip(Math.sqrt(Math.pow(gamepad1.left_stick_x, 2) + Math.pow(gamepad1.left_stick_y, 2)), -1, 1),
+            double POWER = -1 * Range.clip(Math.max(Range.clip(Math.sqrt(Math.pow(gamepad1.left_stick_x, 2) + Math.pow(gamepad1.left_stick_y, 2)), -1, 1),
                     Math.abs(gamepad1.right_stick_x)), -1, 1);
             double maxPower = Math.max(Math.max(Math.abs(gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x),
                     Math.abs(gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x)),
                     Math.max(Math.abs(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x),
                             Math.abs(gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x)));
 
-            motorfrontLeft.setPower(POWER * (gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x) / maxPower);
-            motorbackLeft.setPower(POWER * (gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x) / maxPower);
-            motorfrontRight.setPower(POWER * (gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x) / maxPower);
-            motorbackRight.setPower(POWER * (gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x) / maxPower);
+            fL.setPower(POWER * (gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x) / maxPower);
+            bL.setPower(POWER * (gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x) / maxPower);
+            fR.setPower(POWER * (gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x) / maxPower);
+            bR.setPower(POWER * (gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x) / maxPower);
 
-            telemetry.addData("POWER: ", POWER);
-            telemetry.addData("maxPower: ", maxPower);
+            lS.setPower(0);
 
+            if(gamepad1.right_bumper){
+                lS.setPower(0.5);
+            }
+
+            if(gamepad1.left_bumper){
+                lS.setPower(-0.5);
+            }
+
+            if((!gamepad1.left_bumper) && (!gamepad1.right_bumper)){
+                lS.setPower(0);
+            }
+
+            if(gamepad1.a){
+                left.setPosition(1);
+                right.setPosition(1);
+            }
+            if(!gamepad1.a){
+                left.setPosition(0);
+                right.setPosition(0);
+            }
         }
     }
 }
