73279272d6682405e8262a7ca0de5d307509bab8
kevinwang <PhosphorusElement15@gmail.com>
2017-11-07 16:34:26 -0800

    Updated UltraLord code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_358.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_358.java
@@ -53,9 +53,13 @@ public class TeleOp_358 extends LinearOpMode {
             left.setPosition(0);
             right.setPosition(0);
 
-            while(gamepad1.right_bumper){
+            if(gamepad1.right_bumper){
                 lS.setPower(0.5);
             }
+            if(!gamepad1.right_bumper){
+                lS.setPower(0);
+            }
+
             while(gamepad1.left_bumper){
                 lS.setPower(-0.5);
             }
