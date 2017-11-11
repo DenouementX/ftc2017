6e020a307830651c02c630e3a485c89bb476b99a
Lawrence <DenouementX@users.noreply.github.com>
2017-11-10 20:36:06 -0800

    two controllers for tele_op, gamepad1 controls drive whereas gamepad2 controls glyph movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_358.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_358.java
@@ -61,19 +61,19 @@ public class TeleOp_358 extends LinearOpMode {
 
             lS.setPower(0);
 
-            if(gamepad1.right_bumper){
+            if(gamepad2.right_bumper){
                 lS.setPower(0.5);
             }
 
-            if(gamepad1.left_bumper){
+            if(gamepad2.left_bumper){
                 lS.setPower(-0.5);
             }
 
-            if(gamepad1.a){
+            if(gamepad2.a){
                 left.setPosition(1);
                 right.setPosition(1);
             }
-            if(!gamepad1.a){
+            if(!gamepad2.a){
                 left.setPosition(0);
                 right.setPosition(0);
             }
