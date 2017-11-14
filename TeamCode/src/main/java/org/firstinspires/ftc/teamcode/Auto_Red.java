cfaaf78c758be7263aec163e64639ab9aa9f5b9d
Lawrence <DenouementX@users.noreply.github.com>
2017-11-13 17:25:51 -0800

    Auto_Red is now within a State Machine + Vuforia Initialization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red.java
@@ -1,16 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
 /**
- *  This current autonomous will simply knock off the jewel (30 points)
- *  and park in the safe zone (10 points)
+ *  This current autonomous will knock off the jewel (30 points),
+ *  Decipher the pictograph and place the block in the respective column (45 points),
+ *  Drive into the safe zone (10 points)
+ *  For a total of 85 points (the maximum possible)
  */
 
-//main
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 @Autonomous
 
@@ -26,11 +34,13 @@ public class Auto_Red extends LinearOpMode {
     Servo arm;
     ColorSensor color;
     state state358;
+    VuforiaLocalizer vuforia;
 
     double dPosition = 0.3;
     double oPosition = 1;
+
     enum state {
-        JEWEL, SAFEZONE, GLYPH
+        DECIPHER, JEWEL, SAFEZONE, GLYPH, STOP
     }
 
     public void runOpMode() throws InterruptedException {
@@ -44,7 +54,7 @@ public class Auto_Red extends LinearOpMode {
         right = hardwareMap.servo.get("right");
         arm = hardwareMap.servo.get("arm");
         color = hardwareMap.colorSensor.get("color");
-        state358 = state.JEWEL;
+        state358 = state.DECIPHER;
 
         fL.setDirection(DcMotor.Direction.REVERSE);
         left.setDirection(Servo.Direction.REVERSE);
@@ -56,10 +66,35 @@ public class Auto_Red extends LinearOpMode {
         left.setPosition(0);
         right.setPosition(0);
 
+        //Setting up Camera and Vuforia
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = "AXzW9CD/////AAAAGTPAtr9HRUXZmowtd9p0AUwuXiBVONS/c5x1q8OvjMrQ8/XJGxEp0TP9Kl8PvqSzeXOWIvVa3AeB6MyAQboyW/Pgd/c4a4U/VBs1ouUsVBkEdbaq1iY7RR0cjYr3eLwEt6tmI37Ugbwrd5gmxYvOBQkGqzpbg2U2bVLycc5PkOixu7PqPqaINGZYSlvUzEMAenLOCxZFpsayuCPRbWz6Z9UJfLeAbfAPmmDYoKNXRFll8/jp5Ie7iAhSQgfFggWwyiqMRCFA3GPTsOJS4H1tSiGlMjVzbJnkusPKXfJ0dK3OH9u7ox9ESpi91T0MemXw3nn+/6QRvjGtgFH+wMDuQX7ta89+yW+wqdXX9ZQu8BzY";
+
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate");
+
+        relicTrackables.activate();
+
         waitForStart();
 
         while (opModeIsActive()) {
             switch(state358) {
+
+                //INCOMPLETE: Assign Value to L, M, R perhaps?
+                case DECIPHER:
+                    state358 = state.JEWEL;
+                    RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+                    while (vuMark != RelicRecoveryVuMark.LEFT || vuMark != RelicRecoveryVuMark.CENTER || vuMark !=RelicRecoveryVuMark.RIGHT) {
+                        telemetry.addData("VuMark", "not visible");
+                    }
+                    telemetry.addData("VuMark", "%s visible", vuMark);
+                    break;
 
                 case JEWEL:
                     state358 = state.SAFEZONE;
@@ -81,7 +116,7 @@ public class Auto_Red extends LinearOpMode {
                         arm.setPosition(oPosition);
                     }
 
-                    else{
+                    else {
                         fL.setPower(0);
                         bL.setPower(0);
                         fR.setPower(0);
@@ -99,6 +134,7 @@ public class Auto_Red extends LinearOpMode {
                     break;
 
                 case GLYPH:
+                    state358 = state.STOP;
                     fL.setPower(0);
                     bL.setPower(0);
                     fR.setPower(0);
@@ -106,8 +142,13 @@ public class Auto_Red extends LinearOpMode {
                     sleep(200);
                     break;
 
-            }
+                case STOP:
+                    fL.setPower(0);
+                    bL.setPower(0);
+                    fR.setPower(0);
+                    bR.setPower(0);
 
+            }
         }
     }
 }
