3917bdfee27211796d03549f79410381f8074317
Lawrence <DenouementX@users.noreply.github.com>
2017-11-18 10:05:13 -0800

    removed some imports for aesthetic appeal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red_Helper_2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red_Helper_2.java
@@ -13,13 +13,6 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
-import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
-
 @Autonomous
 
 public class Auto_Red_Helper_2 extends LinearOpMode {
